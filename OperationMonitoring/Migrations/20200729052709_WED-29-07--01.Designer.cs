6bb7e19d65baf529d3d1eca77a3e863482c77b1b
KateSahaidak <kate.sahaidak@gmail.com>
2020-07-29 10:12:55 +0300

    migrations

--- OperationMonitoring/Migrations/20200729052709_WED-29-07--01.Designer.cs
+++ OperationMonitoring/Migrations/20200729052709_WED-29-07--01.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OperationMonitoring.Data;
 
 namespace OperationMonitoring.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20200729052709_WED-29-07--01")]
+    partial class WED290701
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
